9fa776e369708a3bf7374886bc823cd919ad385c
Oleksandr Volynets <vonswork@gmail.com>
2023-02-14 11:49:34 +0100

    feat: static url for request

--- src/common/api/httpClient.ts
+++ src/common/api/httpClient.ts
@@ -1,10 +1,9 @@
 import axios from 'axios'
 import qs from 'qs'
-import config from 'react-native-config'
 import applyLoggerInterceptor from './interceptors/logger'
 
 const httpClient = axios.create({
-  baseURL: config.API_URL,
+  baseURL: 'https://id.gs1.se/',
   paramsSerializer: params => qs.stringify(params, { arrayFormat: 'repeat' }),
 })
 
